f8352bb189ed11bfba95647bde555eae62fd47bd
KMICB <86378435+kmicb@users.noreply.github.com>
2023-06-02 21:11:51 -0400

    added notes

---
+++ ytdl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Features
+#
+# Main Menu; using 'case' maybe? Menu options:
+    # 01 Get URL Info - prompt for url and display data
+    # 02 Download...
+        # Video (as is, with audio and video)
+            # Best Video
+            # 1080p
+            # 720p
+        # Audio Only
+            # Format
+            # Quality
